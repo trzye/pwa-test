718f29b71ae0b6901066e071bc8e64854e8d475a
Michał Jereczek <michaljereczek@gmail.com>
2018-12-21 19:55:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 navigator.serviceWorker.register("serviceworker.js").then(function() {
 		console.log('Service Worker Registered'); 
 });
+
+const installButton = document.querySelector('.install');
+
+let installPrompt = null;
+installButton.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (prompt) => {
+    installPrompt = prompt;
+    installButton.style.display = 'block';
+    installButton.addEventListener('click', (e) => {
+        addBtn.style.display = 'none';
+        installPrompt.prompt();
+        installPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted installation');
+            } else {
+                console.log('user not accepted installation');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
